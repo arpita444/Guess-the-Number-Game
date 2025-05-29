493fd34512f3c63743115ce7091000ef8a14d267
Arpita Behera <arpitabehera22019@gmail.com>
2025-05-29 17:12:53 +0530

    Add files via upload

---
+++ guessthenumber.py
@@ -0,0 +1,40 @@
+import random
+import logo_art
+EASY_LEVEL_ATTEMPTS = 10
+HARD_LEVEL_ATTEMPTS = 5
+def set_difficulty(level_chosen):
+    if level_chosen == "easy":
+        return EASY_LEVEL_ATTEMPTS
+    elif level_chosen == "hard":
+        return HARD_LEVEL_ATTEMPTS
+    else:
+        return 
+def check_answer(guessed_number, answer,attempts):
+    if guessed_number < answer:
+        print("Your guess is too low.")
+        return attempts - 1
+    elif guessed_number > answer:
+        print("Your guess is too high.")
+        return attempts - 1
+    else:
+        print(f"You got it! the answer was{answer}")
+def game():
+    print(logo_art.logo)
+    print("Let me think of a number between 1 and 50.")
+    answer = random.randint(1, 50)
+    level=input("Choose level of difficulty...type 'easy' or 'hard':")
+    attempts=set_difficulty(level)
+    if attempts!= EASY_LEVEL_ATTEMPTS and attempts != HARD_LEVEL_ATTEMPTS:
+        print("Invalid level")
+        return
+    guessed_number = 0
+    while guessed_number != answer:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guessed_number = int(input("Make a guess: "))
+        attempts = check_answer(guessed_number, answer, attempts)
+        if attempts == 0 :
+            print("You've run out of guesses, you lose.")
+            return 
+        elif( guessed_number!=answer):
+            print("Guess again.")
+game()
